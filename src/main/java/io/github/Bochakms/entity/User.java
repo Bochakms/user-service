759dd1c1b7280ba07433d3c75c180e80e1c2aa11
Bochakms <132824494+Bochakms@users.noreply.github.com>
2025-06-26 22:30:45 +0200

    add lombok, add Exceptions, add private constructor

--- src/main/java/io/github/Bochakms/entity/User.java
+++ src/main/java/io/github/Bochakms/entity/User.java
@@ -7,13 +7,14 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "users", schema = "public")
 public class User {
-    @Override
-    public String toString() {
-        return "User{id=" + id + ", name='" + name + "', email='" + email + "', age=" + age + "}";
-    }
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,18 +31,6 @@ public class User {
     
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    // Геттеры и сеттеры
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public Integer getAge() { return age; }
-    public void setAge(Integer age) { this.age = age; }
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }   
+    private LocalDateTime createdAt; 
 
 }
